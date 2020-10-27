491371bef8360f7b58a954bafb7e93148d60c4dd
Carlos <carlosacorti@hotmail.com>
2020-10-27 13:18:07 +0100

    adding done button to edit nickname

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +16,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val nicknameText = binding.nicknameText
+        val nicknameEdit = binding.nicknameEdit
+        nicknameText.text = nicknameEdit.text.toString()
+        nicknameEdit.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+
+        // To hide keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
